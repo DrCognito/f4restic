b77a1fcf338e0ed456bb8e9ea0c77ad02554ff7e
Matt Chadwick <dr.m.chadwick@gmail.com>
2020-02-19 16:59:06 +0000

    Handle directory entry to explicitly get files.

--- src/f4restic/file/discovery.py
+++ src/f4restic/file/discovery.py
@@ -8,4 +8,14 @@ def filtered_files(
 
     glob = pathlib.Path.rglob if recursive else pathlib.Path.glob
 
-    return [str(x) for x in glob(base_dir, pattern)]
+    out = []
+    p: pathlib.Path
+    for p in glob(base_dir, pattern):
+        if p.is_dir() and recursive:
+            # Add all the files in the directory
+            # As we use glob it will perform recursive addition as well!
+            out.extend([str(x) for x in glob(p, "*")])
+        else:
+            out.append(str(p))
+
+    return out
